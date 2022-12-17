d92ffba51a2b4ecec46f69ecba1cfabda19c03e0
ASHUTOSH PAWAR <80522156+gurrudev@users.noreply.github.com>
2022-12-17 23:28:06 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() { 
- // get the input element
- var input = document.getElementById("name"); 
+     // get the input element
+     var input = document.getElementById("name"); 
  
- // add the focus event listener to the input element
- input.addEventListener("focus", function(event) { 
- // get the element with id "john"
- var john = document.getElementById("john"); 
+     // add the focus event listener to the input element
+     input.addEventListener("focus", function(event) { 
+          // get the element with id "john"
+          var john = document.getElementById("john"); 
  
- // get all direct children of "john" with a class of "important"
- var importantElements = john.querySelectorAll(".important"); 
+          // get all direct children of "john" with a class of "important"
+          var importantElements = john.querySelectorAll(".important"); 
  
- // add the "shape" class to all "important" elements
- importantElements.forEach(function(element) { 
- element.classList.add("shape"); 
- }); 
+          // add the "shape" class to all "important" elements
+          importantElements.forEach(function(element) { 
+                   element.classList.add("shape"); 
+          }); 
  
- // stop the event from propagating beyond this handler
- event.stopPropagation(); 
- }); 
+          // stop the event from propagating beyond this handler
+          event.stopPropagation(); 
+      }); 
  }); 
